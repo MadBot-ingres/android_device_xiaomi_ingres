fe4df05fa594ad1a81303e281d1c8176926a7ee8
Mahdi_Asghari <128910022+itisMahdi@users.noreply.github.com>
2025-02-01 15:05:41 +0330

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo 'Starting to clone stuffs needed to build for ingres'
 
 echo 'Cloning vendor ingres'
-git clone https://gitlab.com/skyres17/vendor_xiaomi_ingres -b A15 vendor/xiaomi/ingres
+git clone https://gitlab.com/vherawidatama/vendor_xiaomi_ingres -b 14 vendor/xiaomi/ingres
 
 echo 'Cloning sm8450-common'
-git clone https://github.com/MadBot-ingres/android_device_xiaomi_sm8450-common.git -b lineage-22 device/xiaomi/sm8450-common
+git clone https://github.com/itisMahdi/android_device_xiaomi_sm8450-common.git -b ingres-A15 device/xiaomi/sm8450-common
 
 echo 'Cloning vendor sm8450-common'
-git clone https://github.com/MadBot-ingres/android_vendor_xiaomi_sm8450-common.git -b lineage-22 vendor/xiaomi/sm8450-common
+git clone https://github.com/itisMahdi/android_vendor_xiaomi_sm8450-common.git -b ingres-A15 vendor/xiaomi/sm8450-common
 
 echo 'Cloning hardware xiaomi'
-git clone https://github.com/MadBot-ingres/hardware_xiaomi.git -b lineage-21-new hardware/xiaomi
+git clone https://github.com/itisMahdi/hardware_xiaomi.git -b ingres-A15 hardware/xiaomi
 
 echo 'Cloning ingres-kernel'
-git clone https://github.com/Ingres-Centre/android_device_xiaomi_ingres-kernel.git -b smgreborn device/xiaomi/ingres-kernel
+git clone https://github.com/itisMahdi/android_device_xiaomi_ingres-kernel.git -b ingres-A15 device/xiaomi/ingres-kernel
 
